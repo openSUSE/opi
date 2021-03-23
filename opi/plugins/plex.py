a7568fd04eab4844d1588e6b6dca44d67d145d8e
Dominik Heidler <dheidler@suse.de>
2021-03-23 16:58:26 +0100

    Add Plex plugin

---
+++ opi/plugins/plex.py
@@ -0,0 +1,23 @@
+import opi
+from opi.plugins import BasePlugin
+import subprocess
+
+class PlexMediaServer(BasePlugin):
+	main_query = "plex"
+	description = "Plex Media Server"
+	queries = ('plex', 'plexmediaserver')
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no("Do you want to install plexmediaserver from Plex repository?", 'y'):
+			return
+
+		opi.add_repo(
+			filename = 'PlexRepo',
+			name = 'PlexRepo',
+			url = 'https://downloads.plex.tv/repo/rpm/$basearch/',
+			gpgkey = 'https://downloads.plex.tv/plex-keys/PlexSign.key'
+		)
+
+		subprocess.call(['sudo', 'zypper', 'in', 'plexmediaserver'])
+		opi.ask_keep_repo('PlexRepo')
