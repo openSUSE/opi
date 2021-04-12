59b626402ce0c53699c45941252f227141b18bf2
Jamie Phillips <cphill11@gmail.com>
2021-04-11 21:16:47 -0400

    Adding Support for installing MS Edge
    Currently this only install the dev channel as that is all that is in
    the repository. When the stable channel is released it should be in the
    same repository just like Teams and Code. When that happens the default
    install command can be updated.

---
+++ opi/plugins/ms_edge.py
@@ -0,0 +1,23 @@
+import opi
+from opi.plugins import BasePlugin
+import subprocess
+
+class MSEdge(BasePlugin):
+	main_query = "msedge"
+	description = "Microsoft Edge"
+	queries = ('microsoftedge', 'msedge', 'edge')
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no("Do you want to install Edge from Microsoft repository?", 'y'):
+			return
+
+		opi.add_repo(
+			filename = 'microsoft-edge',
+			name = 'MS Edge',
+			url = 'https://packages.microsoft.com/yumrepos/edge',
+			gpgkey = 'https://packages.microsoft.com/keys/microsoft.asc'
+		)
+
+		subprocess.call(['sudo', 'zypper', 'in', 'microsoft-edge-dev'])
+		opi.ask_keep_repo('edge')
