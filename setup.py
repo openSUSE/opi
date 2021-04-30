2656435ba15689342217ebd67336f1860cfde679
Dominik Heidler <dheidler@suse.de>
2021-04-30 13:04:54 +0200

    Add --version option

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 from distutils.core import setup
+import os
+
+# Load __version__ from opi/version.py
+exec(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "opi/version.py")).read())
 
 setup(
     name='opi',
-    version='2.0.0',
+    version=__version__,
     license='GPLv3',
     description='Tool to Search and install almost all packages available for openSUSE and SLE',
     author='Guo Yunhe, Dominik Heidler',
-    author_email='dheidler@suse.de',
+    author_email='i@guoyunhe.me, dheidler@suse.de',
     requires=['lxml', 'requests', 'termcolor'],
     packages=['opi', 'opi.plugins'],
     scripts=['bin/opi'],
