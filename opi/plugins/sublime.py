410ac14a00729d74065736d3c4e041b9a4c8f2b0
Марков Дмитрий <13werwolf13@mail.ru>
2021-09-03 16:07:52 +0600

    add sublime, yandex-disk (#66)
    * add sublime, yandex-disk
    
    Co-authored-by: Дмитрий Марков <markov@promobit.ru>

---
+++ opi/plugins/sublime.py
@@ -0,0 +1,23 @@
+import opi
+from opi.plugins import BasePlugin
+import subprocess
+
+class SublimeText(BasePlugin):
+	main_query = "sublime"
+	description = "Editor for code, markup and prose"
+	queries = ('sublime', 'sublime-text', 'sublimetext')
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no("Do you want to install sublime-text from sublime-text repository?", 'y'):
+			return
+
+		opi.add_repo(
+			filename = 'sublime-text',
+			name = 'sublime-text',
+			url = 'https://download.sublimetext.com/rpm/stable/x86_64',
+			gpgkey = 'https://download.sublimetext.com/sublimehq-rpm-pub.gpg'
+		)
+
+		opi.install_packages(['sublime-text'])
+		opi.ask_keep_repo('sublime-text')
