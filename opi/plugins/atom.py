a80f44f9bd084433d3f475796b3edee0162ea6bd
giovcandido <giovcandido@outlook.com>
2021-09-10 16:29:54 -0300

    Create atom text editor plugin

---
+++ opi/plugins/atom.py
@@ -0,0 +1,24 @@
+import opi
+
+from opi.plugins import BasePlugin
+
+class Atom(BasePlugin):
+	main_query = "atom"
+	description = "Atom Text Editor"
+	queries = ('atom', 'atom-editor')
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no("Do you want to install Atom from Atom repository?", 'y'):
+			return
+
+		opi.add_repo(
+			filename = 'atom',
+			name = 'atom',
+			url = 'https://packagecloud.io/AtomEditor/atom/el/7/x86_64/?type=rpm',
+			gpgkey = 'https://packagecloud.io/AtomEditor/atom/gpgkey'
+		)
+
+		opi.install_packages(['atom'])
+
+		opi.ask_keep_repo('atom')
