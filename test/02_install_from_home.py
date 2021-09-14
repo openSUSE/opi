c643d6e96457b21565569f2e181c420943eb84bc
giovcandido <giovcandido@outlook.com>
2021-09-14 10:39:00 -0300

    Update tests to work with new changes

--- test/02_install_from_home.py
+++ test/02_install_from_home.py
@@ -6,24 +6,25 @@ import subprocess
 
 c = pexpect.spawn('./bin/opi xosview', logfile=sys.stdout.buffer, echo=False)
 
-c.expect("1. xosview\r\n")
-c.expect('Choose a number')
+c.expect('1. xosview\r\n')
+c.sendline('q')
+c.expect('Pick a number')
 c.sendline('1')
 
-c.expect("2. .*X11:Utilities", timeout=10)
+c.expect('2. .*X11:Utilities', timeout=10)
 c.sendline('2')
 
-c.expect("new packages to install", timeout=60)
-c.expect("Continue", timeout=60)
+c.expect('new packages to install', timeout=60)
+c.expect('Continue', timeout=60)
 c.sendline('y')
 
-c.expect("Do you want to keep the repo", timeout=350)
+c.expect('Do you want to keep the repo', timeout=350)
 c.sendline('n')
 
 c.interact()
 c.wait()
 c.close()
-assert c.exitstatus == 0, "Exit code: %i" % c.exitstatus
+assert c.exitstatus == 0, 'Exit code: %i' % c.exitstatus
 
 subprocess.check_call(['rpm', '-qi', 'xosview'])
 subprocess.check_call('zypper lr -u | grep -v X11', shell=True)
