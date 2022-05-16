36b44ba99f7aad868c069e5d81b9ccfd6b2c175c
Dominik Heidler <dheidler@suse.de>
2022-05-16 14:20:14 +0200

    Fix 02_install_from_home.py test for leap

--- test/02_install_from_home.py
+++ test/02_install_from_home.py
@@ -4,17 +4,19 @@ import sys
 import pexpect
 import subprocess
 
-c = pexpect.spawn('./bin/opi xosview', logfile=sys.stdout.buffer, echo=False)
+c = pexpect.spawn('./bin/opi rtl8812au', logfile=sys.stdout.buffer, echo=False)
 
-c.expect('1. xosview\r\n')
+c.expect('1. rtl8812au\r\n')
 c.sendline('q')
 c.expect('Pick a number')
 c.sendline('1')
 
-c.expect('2. .*X11:Utilities', timeout=10)
-c.sendline('2')
+c.expect(r'([0-9]+)\. [^ ]*hardware', timeout=10)
+hwentryid = c.match.groups()[0]
+print("PEXPECT: Found hardware entry id %r" % hwentryid)
+c.sendline(hwentryid)
 
-c.expect('new packages to install', timeout=60)
+c.expect('new packages? to install', timeout=60)
 c.expect('Continue', timeout=60)
 c.sendline('y')
 
@@ -26,5 +28,5 @@ c.wait()
 c.close()
 assert c.exitstatus == 0, 'Exit code: %i' % c.exitstatus
 
-subprocess.check_call(['rpm', '-qi', 'xosview'])
-subprocess.check_call('zypper lr -u | grep -v X11', shell=True)
+subprocess.check_call(['rpm', '-qi', 'rtl8812au'])
+subprocess.check_call('zypper lr -u | grep -v hardware', shell=True)
