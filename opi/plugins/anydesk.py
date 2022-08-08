56db121ff86bd7f01f2bd30682508849c6439a29
Nakadzima <pascal.german@yandex.ru>
2022-08-08 14:14:43 +0600

    refactoring code

--- opi/plugins/anydesk.py
+++ opi/plugins/anydesk.py
@@ -2,7 +2,6 @@ import opi
 from opi.plugins import BasePlugin
 import subprocess
 
-
 class AnyDesk(BasePlugin):
     main_query = "anydesk"
     description = "Anydesk remote access"
@@ -19,5 +18,6 @@ class AnyDesk(BasePlugin):
             url = 'http://rpm.anydesk.com/opensuse/$basearch/',
             gpgkey = 'https://keys.anydesk.com/repos/RPM-GPG-KEY'
         )
+
         opi.install_packages(['anydesk'])
         opi.ask_keep_repo('anydesk')
