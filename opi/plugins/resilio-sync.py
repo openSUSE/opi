a6337103400725e266d6aac88e09b2e436d3cd41
Дмитрий Марков <13werwolf13@mail.ru>
2022-09-02 22:48:05 +0500

    add resilio-sync

---
+++ opi/plugins/resilio-sync.py
@@ -0,0 +1,24 @@
+import opi
+from opi.plugins import BasePlugin
+import subprocess
+
+class ResilioSync(BasePlugin):
+	main_query = "resilio-sync"
+	description = "Resilio Sync decentralized file synchronization between devices using the bittorrent protocol."
+	queries = ['resilio-sync, resilio, rslsync']
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no("Do you want to install resilio-sync from resilio-sync repository?", 'y'):
+			return
+
+		opi.add_repo(
+			filename = 'resilio-sync',
+			name = 'resilio-sync',
+			url = 'https://linux-packages.resilio.com/resilio-sync/rpm/$basearch',
+			gpgkey = 'https://linux-packages.resilio.com/resilio-sync/key.asc',
+			gpgcheck = False
+		)
+
+		opi.install_packages(['resilio-sync'])
+		opi.ask_keep_repo('resilio-sync')
