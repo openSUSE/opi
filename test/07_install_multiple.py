caff2cd24cb172931db025895ebd501dcdb00090
nobkd <44443899+nobkd@users.noreply.github.com>
2023-10-10 23:30:04 +0200

    test: run multi install in non interactive mode

--- test/07_install_multiple.py
+++ test/07_install_multiple.py
@@ -4,41 +4,38 @@ import sys
 import pexpect
 import subprocess
 
-c = pexpect.spawn('./bin/opi -m android-tools yandex-disk', logfile=sys.stdout.buffer, echo=False)
+c = pexpect.spawn('./bin/opi -nm zfs resilio-sync html2text yandex-disk', logfile=sys.stdout.buffer, echo=False)
 
 # plugins are installed first
-c.expect('Do you want to install')
-c.sendline('y')
-
+c.expect('Do you want to install resilio-sync')
 c.expect('Import package signing key', timeout=10)
-c.sendline('y')
-
 c.expect('Continue')
-c.sendline('y')
-
 c.expect('Do you want to keep', timeout=500)
-c.sendline('y')
+
+c.expect('Do you want to install yandex-disk')
+c.expect('Import package signing key', timeout=10)
+c.expect('Continue')
+c.expect('Do you want to keep', timeout=500)
 
 # packages come after plugins
-c.expect(r'([0-9]+)\. android-tools', timeout=10)
-entry_id = c.match.groups()[0]
-print(f'PEXPECT: Found entry id {entry_id!r}')
+c.expect(r'([0-9]+)\. zfs', timeout=10)
 c.expect('Pick a number')
-c.sendline(entry_id)
+c.expect(r'([0-9]+)\. [^ ]*(filesystems)', timeout=10)
+c.expect('Adding repo \'filesystems\'', timeout=10)
+c.expect('Continue?', timeout=20)
 
+c.expect(r'([0-9]+)\. html2text', timeout=10)
+c.expect('Pick a number')
 c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=10)
-entry_id = c.match.groups()[0]
-print(f'PEXPECT: Found entry id {entry_id!r}')
-c.sendline(entry_id)
-
 c.expect('Installing from existing repo', timeout=10)
 c.expect('Continue?', timeout=20)
-c.sendline('y')
 
 c.interact()
 c.wait()
 c.close()
 print()
 assert c.exitstatus == 0, f'Exit code: {c.exitstatus}'
+subprocess.check_call(['rpm', '-qi', 'resilio-sync'])
 subprocess.check_call(['rpm', '-qi', 'yandex-disk'])
-subprocess.check_call(['rpm', '-qi', 'android-tools'])
+subprocess.check_call(['rpm', '-qi', 'zfs'])
+subprocess.check_call(['rpm', '-qi', 'html2text'])
