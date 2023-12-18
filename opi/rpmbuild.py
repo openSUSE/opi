openSUSE/opi

commit 6a86965faed9b2aa0ef6e4dc0b021f0a8f1b848e

Add Snap library and Spotify plugin

opi/rpmbuild.py
@@ -1,81 +1,117 @@
 import os
 import tempfile
-import textwrap
 import re
 import subprocess
 import glob
+import shutil
+
+def copy(src, dst):
+	"""
+		Copy src to dst using hardlinks.
+		dst will be the full final path.
+		Directories will be created as needed.
+	"""
+	if os.path.islink(src) or os.path.isfile(src):
+		os.makedirs(os.path.dirname(dst), exist_ok=True)
+	if os.path.islink(src):
+		link_target = os.readlink(src)
+		os.symlink(link_target, dst)
+	elif os.path.isfile(src):
+		shitil.copy2(src, dst)
+	else:
+		shutil.copytree(src, dst, copy_function=os.link, symlinks=True, ignore_dangling_symlinks=False)
+
+def dedent(s):
+	""" Other than textwrap's implementation this one has no problems with some lines unindented.
+	    It will unconditionally strip any leading whitespaces for each line.
+	"""
+	return re.sub(r"^\s*", "", s, flags=re.M)
 
 class RPMBuild:
-	def __init__(self, name, version, description, buildarch="noarch", files=[], dirs=[], config=[]):
+	def __init__(self, name, version, description, buildarch="noarch",
+	             requires=[], recommends=[], provides=[], suggests=[], conflicts=[], autoreq=True,
+		         files=[], dirs=[], config=[]):
 		self.name = name
 		self.version = version
 		self.description = description
 		self.buildarch = buildarch
+		self.requires = requires
+		self.recommends = recommends
+		self.provides = provides
+		self.suggests = suggests
+		self.conflicts = conflicts
+		self.autoreq = autoreq
 		self.files = files
 		self.dirs = dirs
 		self.config = config
 
 		self.tmpdir = tempfile.TemporaryDirectory()
 
-		self.src_root_dir = os.path.join(self.tmpdir.name, "root")
+		self.buildroot = os.path.join(self.tmpdir.name, "buildroot") # buildroot where plugins copy files to
 		self.spec_path = os.path.join(self.tmpdir.name, "specfile.spec")
-		self.tmp_buildroot_dir = os.path.join(self.tmpdir.name, "buildroot")
+		self.rpmbuild_buildroot = os.path.join(self.tmpdir.name, "rpmbuildroot") # buildroot internally used by rpmbuild
 		self.rpm_out_dir = os.path.join(self.tmpdir.name, "rpms")
 
-		os.mkdir(self.src_root_dir)
+		os.mkdir(self.buildroot)
 		os.mkdir(self.rpm_out_dir)
 
-	@staticmethod
-	def _mkspec(name, version, description, buildarch="noarch", files=[], dirs=[], config=[]):
+	def mkspec(self):
 		nl = "\n"
-		spec = re.sub(r"^\s*", "", f"""
-			Name:      {name}
-			Version:   {version}
+		spec = dedent(f"""
+			Name:      {self.name}
+			Version:   {self.version}
 			Release:   0
-			Summary:   {description}
+			Summary:   {self.description}
 			License:   n/a
-			BuildArch: {buildarch}
+			BuildArch: {self.buildarch}
+			{nl.join(f"Requires: {r}" for r in self.requires)}
+			{nl.join(f"Recommends: {r}" for r in self.recommends)}
+			{nl.join(f"Provides: {r}" for r in self.provides)}
+			{nl.join(f"Suggests: {r}" for r in self.suggests)}
+			{nl.join(f"Conflicts: {r}" for r in self.conflicts)}
+			{"AutoReq: no" if not self.autoreq else ''}
 
 			%description
-			{description}
+			{self.description}
 			Built locally using OPI.
 
 			%install
-			cp -lav ./root/* %{{buildroot}}/
+			cp -lav ./buildroot/* %{{buildroot}}/
 
 			%files
-			{nl.join(files)}
-			{nl.join(f"%dir {d}" for d in dirs)}
-			{nl.join(f"%config {c}" for c in config)}
+			{nl.join(self.files)}
+			{nl.join(f"%dir {d}" for d in self.dirs)}
+			{nl.join(f"%config {c}" for c in self.config)}
 
 			%changelog
-		""", flags=re.M)
+		""")
 		return spec
 
-	def add_desktop_file(self, cmd, icon):
-		os.makedirs(os.path.join(self.src_root_dir, 'usr/share/applications'))
+	def add_desktop_file(self, cmd, icon, **kwargs):
+		os.makedirs(os.path.join(self.buildroot, 'usr/share/applications'))
 		desktop_path = f'usr/share/applications/{self.name}.desktop'
-		desktop_abspath = os.path.join(self.src_root_dir, desktop_path)
+		desktop_abspath = os.path.join(self.buildroot, desktop_path)
 		self.files.append(f"/{desktop_path}")
 		with open(desktop_abspath, 'w') as f:
-			f.write(textwrap.dedent(f"""
+			nl = "\n"
+			f.write(dedent(f"""
 				[Desktop Entry]
 				Name={self.name}
 				Comment={self.description}
 				Exec={cmd}
 				Icon={icon}
 				Type=Application
+				{nl.join(["%s=%s" % (k, v) for k, v in kwargs.items()])}
 			"""))
 
 	def build(self):
 		print(f"Creating RPM for {self.name}")
 		with open(self.spec_path, 'w') as f:
-			spec = type(self)._mkspec(self.name, self.version, self.description, self.buildarch,
-			                          self.files, self.dirs, self.config)
+			spec = self.mkspec()
 			f.write(spec)
 		subprocess.check_call([
 			"rpmbuild", "-bb", "--build-in-place",
-			"--buildroot", self.tmp_buildroot_dir,
+			"--buildroot", self.rpmbuild_buildroot,
 			"--define", f"_rpmdir {self.rpm_out_dir}",
 			"specfile.spec"
 		], cwd=self.tmpdir.name)

opi/github.py
@@ -6,27 +6,36 @@ def http_get_json(url):
 	r.raise_for_status()
 	return r.json()
 
-def get_releases(org, repo, filter_prereleases=True):
+def get_releases(org, repo, filter_prereleases=True, filters=[]):
 	releases = http_get_json(f'https://api.github.com/repos/{org}/{repo}/releases')
 	if filter_prereleases:
 		releases = [release for release in releases if not release['prerelease']]
+	for f in filters:
+		releases = [r for r in releases if f(r)]
 	return releases
+
+def get_latest_release(org, repo, filter_prereleases=True, filters=[]):
+	releases = get_releases(org, repo, filter_prereleases, filters)
+	return releases[0] if releases else None
 
 def get_release_assets(release):
 	return [{'name': a['name'], 'url': a['browser_download_url']} for a in http_get_json(release['assets_url'])]
 
+def get_release_asset(release, filters=[]):
+	assets = get_release_assets(release)
+	for f in filters:
+		assets = [r for r in assets if f(r)]
+	return assets[0] if assets else None
+
 def install_rpm_release(org, repo, allow_unsigned=False):
-	releases = get_releases(org, repo)
-	if not releases:
+	latest_release = get_latest_release(org, repo)
+	if not latest_release:
 		print(f'No release found for {org}/{repo}')
 		return
-	latest_release = releases[0]
 	if not opi.ask_yes_or_no(f"Do you want to install {repo} release {latest_release['tag_name']} RPM from {org} github repo?"):
 		return
-	assets = get_release_assets(latest_release)
-	assets = [a for a in assets if a['name'].endswith('.rpm')]
-	if not assets:
+	asset = get_release_asset(latest_release, filters=[lambda a: a['name'].endswith('.rpm')])
+	if not asset:
 		print(f"No RPM asset found for {org}/{repo} release {latest_release['tag_name']}")
 		return
-	rpm_url = assets[0]['url']
-	opi.install_packages([rpm_url], allow_unsigned=allow_unsigned)
+	opi.install_packages([asset['url']], allow_unsigned=allow_unsigned)

opi/plugins/orca_slicer.py
@@ -14,35 +14,32 @@ class OrcaSlicer(BasePlugin):
 	def run(cls, query):
 		org = 'SoftFever'
 		repo = 'OrcaSlicer'
-		releases = github.get_releases(org, repo)
-		releases = [r for r in releases if not 'beta' in r['tag_name']]
-		if not releases:
+		latest_release = github.get_latest_release(org, repo)
+		if not latest_release:
 			print(f'No release found for {org}/{repo}')
 			return
-		latest_release = releases[0]
-		if not opi.ask_yes_or_no(f"Do you want to install {repo} release {latest_release['tag_name']} from {org} github repo?"):
+		version = latest_release['tag_name'].lstrip('v').replace('-', '_')
+		if not opi.ask_yes_or_no(f"Do you want to install {repo} release {version} from {org} github repo?"):
 			return
-		version = latest_release['tag_name'].lstrip('v')
-		assets = github.get_release_assets(latest_release)
-		assets = [a for a in assets if a['name'].endswith('.AppImage')]
-		if not assets:
-			print(f"No asset found for {org}/{repo} release {latest_release['tag_name']}")
+		asset = github.get_release_asset(latest_release, filters=[lambda a: a['name'].endswith('.AppImage')])
+		if not asset:
+			print(f"No asset found for {org}/{repo} release {version}")
 			return
-		url = assets[0]['url']
+		url = asset['url']
 
 		binary_path = 'usr/bin/OrcaSlicer'
 		icon_path = 'usr/share/pixmaps/OrcaSlicer.svg'
 
-		rpm = rpmbuild.RPMBuild('OrcaSlicer', version, cls.description, "x86_64", [
+		rpm = rpmbuild.RPMBuild('OrcaSlicer', version, cls.description, "x86_64", files=[
 			f"/{binary_path}",
 			f"/{icon_path}"
 		])
 
-		binary_abspath = os.path.join(rpm.src_root_dir, binary_path)
+		binary_abspath = os.path.join(rpm.buildroot, binary_path)
 		http.download_file(url, binary_abspath)
 		os.chmod(binary_abspath, 0o755)
 
-		icon_abspath = os.path.join(rpm.src_root_dir, icon_path)
+		icon_abspath = os.path.join(rpm.buildroot, icon_path)
 		icon_url = "https://raw.githubusercontent.com/SoftFever/OrcaSlicer/2d849f/resources/images/OrcaSlicer.svg"
 		http.download_file(icon_url, icon_abspath)
 

opi/plugins/spotify.py
@@ -1,0 +1,70 @@
+import os
+import opi
+import shutil
+from opi.plugins import BasePlugin
+from opi import rpmbuild
+from opi import snap
+from opi import http
+
+class Spotify(BasePlugin):
+	main_query = 'spotify'
+	description = 'Listen to music for a monthly fee'
+	queries = [main_query]
+
+	@classmethod
+	def run(cls, query):
+		s = snap.get_snap('spotify')
+		if not opi.ask_yes_or_no(f"Do you want to install spotify release {s['version']} converted to RPM from snapcraft repo?"):
+			return
+
+		binary_path = 'usr/bin/spotify'
+		data_path = 'usr/share/spotify'
+
+		rpm = rpmbuild.RPMBuild('spotify', s['version'], cls.description, "x86_64", 
+			conflicts = ["spotify-client"],
+			requires = [
+				"libasound2",
+				"libatk-bridge-2_0-0",
+				"libatomic1",
+				"libcurl4",
+				"libgbm1",
+				"libglib-2_0-0",
+				"libgtk-3-0",
+				"mozilla-nss",
+				"libopenssl1_1",
+				"libxshmfence1",
+				"libXss1",
+				"libXtst6",
+				"xdg-utils",
+				"libayatana-appindicator3-1",
+			],
+			autoreq = False,
+			recommends = [
+				"libavcodec.so",
+				"libavformat.so",
+			],
+			suggests = ["libnotify4"],
+			files = [
+				f"/{binary_path}",
+				f"/{data_path}"
+			]
+		)
+
+		snap_path = os.path.join(rpm.tmpdir.name, 'spotify.snap')
+		snap_path_extracted = os.path.join(rpm.tmpdir.name, 'spotify')
+		http.download_file(s['url'], snap_path)
+		snap.extract_snap(snap_path, snap_path_extracted)
+
+		binary_abspath = os.path.join(rpm.buildroot, binary_path)
+		rpmbuild.copy(os.path.join(snap_path_extracted, binary_path), binary_abspath)
+
+		data_abspath = os.path.join(rpm.buildroot, data_path)
+		rpmbuild.copy(os.path.join(snap_path_extracted, data_path), data_abspath)
+
+		rpm.add_desktop_file(cmd="spotify %U", icon="/usr/share/spotify/icons/spotify_icon.ico",
+		                     Categories="Audio;Music;Player;AudioVideo;",
+		                     MimeType="x-scheme-handler/spotify"
+		)
+
+		rpm.build()
+		opi.install_packages([rpm.rpmfile_path], allow_unsigned=True)

opi/snap.py
@@ -1,0 +1,20 @@
+import subprocess
+import requests
+import opi
+
+def http_get_json(url):
+	r = requests.get(url, headers={'Snap-Device-Series': '16'})
+	r.raise_for_status()
+	return r.json()
+
+def get_snap(snap, channel='stable', arch=None):
+	channels = http_get_json(f'https://api.snapcraft.io/v2/snaps/info/{snap}')['channel-map']
+	if arch:
+		arch.replace('x86_64', 'amd64')
+		channels = [c for c in channels if c['channel']['architecture'] == arch]
+	channels = [c for c in channels if c['channel']['name'] == channel]
+	c = channels[0]
+	return {"version": c['version'], "url": c['download']['url']}
+
+def extract_snap(snap, target_dir):
+	subprocess.check_call(['unsquashfs', '-d', target_dir, snap])

commit b4fd8090984d7d5b3215f9c5481120127e8d047a

Fix typo in rpmbuild.py
Just a simple typo fix that was introduced in 6a86965

opi/rpmbuild.py
@@ -17,7 +17,7 @@ def copy(src, dst):
 		link_target = os.readlink(src)
 		os.symlink(link_target, dst)
 	elif os.path.isfile(src):
-		shitil.copy2(src, dst)
+		shutil.copy2(src, dst)
 	else:
 		shutil.copytree(src, dst, copy_function=os.link, symlinks=True, ignore_dangling_symlinks=False)
 