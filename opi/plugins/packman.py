3e44ae6b7d848d97b03265be9e3cf3137ef88ad0
Dominik Heidler <dheidler@suse.de>
2024-01-15 13:45:46 +0100

    Codecs: Install AV1 decoder for mpv

--- opi/plugins/packman.py
+++ opi/plugins/packman.py
@@ -32,6 +32,7 @@ class PackmanCodecsPlugin(BasePlugin):
 			'gstreamer-plugins-good-extra',
 			'gstreamer-plugins-bad',
 			'gstreamer-plugins-ugly',
+			'dav1d',
 		])
 
 		if not opi.ask_yes_or_no('Do you want to install openh264 codecs from openSUSE openh264 repository?'):
