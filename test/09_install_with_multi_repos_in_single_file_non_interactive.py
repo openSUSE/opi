3b3f42588c98b6e1a2c45a71a780e8a4463c56a5
Dominik Heidler <dheidler@suse.de>
2024-12-02 16:12:44 +0100

    Fix 09_install_with_multi_repos_in_single_file_non_interactive.py

--- test/09_install_with_multi_repos_in_single_file_non_interactive.py
+++ test/09_install_with_multi_repos_in_single_file_non_interactive.py
@@ -8,9 +8,9 @@ subprocess.check_call("cat /etc/zypp/repos.d/*.repo > /tmp/singlefile.repo", she
 subprocess.check_call("rm -v /etc/zypp/repos.d/*.repo", shell=True)
 subprocess.check_call("mv -v /tmp/singlefile.repo /etc/zypp/repos.d/", shell=True)
 
-c = pexpect.spawn('./bin/opi -n html2text', logfile=sys.stdout.buffer, echo=False)
+c = pexpect.spawn('./bin/opi -n tmux', logfile=sys.stdout.buffer, echo=False)
 
-c.expect(r'([0-9]+)\. html2text', timeout=10)
+c.expect(r'([0-9]+)\. tmux', timeout=10)
 c.expect('Pick a number')
 c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=10)
 c.expect('Installing from existing repo', timeout=10)
@@ -20,7 +20,7 @@ c.wait()
 c.close()
 print()
 assert c.exitstatus == 0, f'Exit code: {c.exitstatus}'
-subprocess.check_call(['rpm', '-qi', 'html2text'])
+subprocess.check_call(['rpm', '-qi', 'tmux'])
 
 
 c = pexpect.spawn('./bin/opi -n zfs', logfile=sys.stdout.buffer, echo=False)
