ff6ae358388418f0e6e5c6a6dc0992ea288c22a1
Dominik Heidler <dheidler@suse.de>
2024-12-02 16:03:19 +0100

    Fix 07_install_multiple.py on tumbleweed

--- test/07_install_multiple.py
+++ test/07_install_multiple.py
@@ -4,7 +4,7 @@ import sys
 import pexpect
 import subprocess
 
-c = pexpect.spawn('./bin/opi -nm zfs resilio-sync html2text', logfile=sys.stdout.buffer, echo=False)
+c = pexpect.spawn('./bin/opi -nm zfs resilio-sync tmux', logfile=sys.stdout.buffer, echo=False)
 
 # plugins are installed first
 c.expect('Do you want to install')
@@ -19,7 +19,7 @@ c.expect(r'([0-9]+)\. [^ ]*(filesystems)', timeout=10)
 c.expect('Adding repo \'filesystems\'', timeout=10)
 c.expect('Continue?', timeout=60)
 
-c.expect(r'([0-9]+)\. html2text', timeout=60)
+c.expect(r'([0-9]+)\. tmux', timeout=60)
 c.expect('Pick a number')
 c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=10)
 c.expect('Installing from existing repo', timeout=10)
@@ -32,4 +32,4 @@ print()
 assert c.exitstatus == 0, f'Exit code: {c.exitstatus}'
 subprocess.check_call(['rpm', '-qi', 'resilio-sync'])
 subprocess.check_call(['rpm', '-qi', 'zfs'])
-subprocess.check_call(['rpm', '-qi', 'html2text'])
+subprocess.check_call(['rpm', '-qi', 'tmux'])
