67d1c10ee08c1fea03aada1adb065c8fc4098983
Dominik Heidler <dheidler@suse.de>
2024-12-02 15:33:16 +0100

    Fix test suite on tumbleweed

--- test/06_install_non_interactive.py
+++ test/06_install_non_interactive.py
@@ -4,9 +4,9 @@ import sys
 import pexpect
 import subprocess
 
-c = pexpect.spawn('./bin/opi -n html2text', logfile=sys.stdout.buffer, echo=False)
+c = pexpect.spawn('./bin/opi -n bottom', logfile=sys.stdout.buffer, echo=False)
 
-c.expect(r'([0-9]+)\. html2text', timeout=10)
+c.expect(r'([0-9]+)\. bottom', timeout=10)
 c.expect('Pick a number')
 c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=10)
 c.expect('Installing from existing repo', timeout=10)
@@ -16,7 +16,7 @@ c.wait()
 c.close()
 print()
 assert c.exitstatus == 0, f'Exit code: {c.exitstatus}'
-subprocess.check_call(['rpm', '-qi', 'html2text'])
+subprocess.check_call(['rpm', '-qi', 'bottom'])
 
 
 c = pexpect.spawn('./bin/opi -n zfs', logfile=sys.stdout.buffer, echo=False)
