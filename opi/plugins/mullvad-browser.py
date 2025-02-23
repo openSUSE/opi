ba522e9ebf1779aa8fa53818a606de4bc7547132
zeussuse <193160247+zeussuse@users.noreply.github.com>
2025-02-23 14:51:34 +0000

    Add mullvad-brower

---
+++ opi/plugins/mullvad-browser.py
@@ -0,0 +1,24 @@
+import opi
+from opi.plugins import BasePlugin
+import subprocess
+
+class MullvadBrowser(BasePlugin):
+	main_query = 'mullvad-browser'
+	description = 'Mullvad web browser'
+	queries = ['mullvad', 'mullvad-browser']
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no('Do you want to install mullvad-browser from Mullvad repository?'):
+			return
+
+		opi.add_repo(
+			filename = 'mullvad',
+			name = 'Mullvad VPN',
+			url = 'https://repository.mullvad.net/rpm/stable/$basearch/',
+                        gpgkey = 'https://repository.mullvad.net/rpm/mullvad-keyring.asc',
+                        gpgcheck = 1
+		)
+
+		opi.install_packages(['mullvad-browser'])
+		opi.ask_keep_repo('mullvad')
