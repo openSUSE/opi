f5c8cc71f167a1104e6652830a23a2d0a3ae4284
munix9 <44939650+munix9@users.noreply.github.com>
2025-05-24 13:37:17 +0200

    Add rustdesk plugin

---
+++ opi/plugins/rustdesk.py
@@ -0,0 +1,16 @@
+import opi
+from opi.plugins import BasePlugin
+from opi import github
+
+class RustDesk(BasePlugin):
+	main_query = 'rustdesk'
+	description = 'An open-source remote desktop application'
+	queries = ['rustdesk', 'RustDesk']
+
+	@classmethod
+	def run(cls, query):
+		arch = opi.get_cpu_arch()
+		github.install_rpm_release('rustdesk', 'rustdesk',
+			filters=[lambda a: a['name'].endswith(f'{arch}-suse.rpm')],
+			allow_unsigned=True # no key available
+		)
