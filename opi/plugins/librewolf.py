1216ac4fd6bbd3ed396697792fd00c726f5c0abf
Дмитрий Марков <13werwolf13@mail.ru>
2025-05-28 23:54:54 +0500

    add librewolf plugin

---
+++ opi/plugins/librewolf.py
@@ -0,0 +1,22 @@
+import opi
+from opi.plugins import BasePlugin
+
+class librewolf(BasePlugin):
+	main_query = 'librewolf'
+	description = 'librewolf'
+	queries = ['librewolf', 'Librewolf', 'LibreWolf']
+
+	@classmethod
+	def run(cls, query):
+		if not opi.ask_yes_or_no('Do you want to install librewolf from librewolf repository?'):
+			return
+
+		opi.add_repo(
+			filename = 'librewolf',
+			name = 'librewolf',
+			url = 'https://rpm.librewolf.net',
+			gpgkey = 'https://rpm.librewolf.net/pubkey.gpg'
+		)
+
+		opi.install_packages(['librewolf'])
+		opi.ask_keep_repo('librewolf')
