2d4e52c5d564aafa867e75cf1d67f24b6a66a466
Dominik Heidler <dheidler@suse.de>
2025-06-02 15:57:02 +0200

    Increase timeouts in test/06_install_non_interactive.py

--- test/06_install_non_interactive.py
+++ test/06_install_non_interactive.py
@@ -6,10 +6,10 @@ import subprocess
 
 c = pexpect.spawn('./bin/opi -n bottom', logfile=sys.stdout.buffer, echo=False)
 
-c.expect(r'([0-9]+)\. bottom', timeout=10)
+c.expect(r'([0-9]+)\. bottom', timeout=20)
 c.expect('Pick a number')
-c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=10)
-c.expect('Installing from existing repo', timeout=10)
+c.expect(r'([0-9]+)\. [^ ]*(openSUSE-Tumbleweed-Oss|Main Repository)', timeout=20)
+c.expect('Installing from existing repo', timeout=20)
 c.expect('Continue?', timeout=60)
 c.interact()
 c.wait()
@@ -21,10 +21,10 @@ subprocess.check_call(['rpm', '-qi', 'bottom'])
 
 c = pexpect.spawn('./bin/opi -n helloworld-opi-tests', logfile=sys.stdout.buffer, echo=False)
 
-c.expect(r'([0-9]+)\. helloworld-opi-tests', timeout=10)
+c.expect(r'([0-9]+)\. helloworld-opi-tests', timeout=20)
 c.expect('Pick a number')
-c.expect(r'([0-9]+)\. [^ ]*(home:dheidler:opitests)', timeout=10)
-c.expect('Adding repo \'home:dheidler:opitests\'', timeout=10)
+c.expect(r'([0-9]+)\. [^ ]*(home:dheidler:opitests)', timeout=20)
+c.expect('Adding repo \'home:dheidler:opitests\'', timeout=20)
 c.expect('Continue?', timeout=60)
 c.interact()
 c.wait()
